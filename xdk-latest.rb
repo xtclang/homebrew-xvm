05bf4b1467380663761558fd3df8cabf6295b2d0
Jenkins <jenkins@x86vm.xtclang.org>
2022-06-10 09:22:55 -0400

    uploadXdk

--- xdk-latest.rb
+++ xdk-latest.rb
@@ -3,7 +3,7 @@ class XdkLatest < Formula
   homepage "https://github.com/xtclang/xvm/"
   url "http://xtclang.org/downloads/xdk-0.4.2ci14+5922b1c54c0970eb89fed13e125d0c1d4713fb08.tar.gz"
   sha256 "7f964934b66a5b3cc8d0f44b5f1332fdfed1a38b070e4249af8e0c672ae42ffc"
-  version "0.4.2ci14"
+  version "0.4.2.14"
   license "Apache-2.0"
 
   depends_on "openjdk"
