10544d873969c88a8463463345de6fd4425145fd
Gene Gleyzer <gene@xqiz.it>
2022-06-10 11:17:12 -0400

    autocommit

--- xdk-latest.rb
+++ xdk-latest.rb
@@ -1,7 +1,7 @@
 class Xdk-latest < Formula
   desc "Ecstasy Development Kit (XDK)"
   homepage "https://github.com/xtclang/xvm/"
-  url "http://xtclang.org/downloads/xdk-0.4.1.tar.gz"
+  url "http://xtclang.org/downloads/xdk-0.4.1ci1.tar.gz"
   sha256 "4e3f43a0149fdb725f1f6ffda2e5b9b33f59b1dcb9645024ea06fe8266b97856"
   license "Apache-2.0"
 
