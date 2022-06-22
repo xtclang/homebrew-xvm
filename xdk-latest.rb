c27e01d928224561246e9b290ae02720c0d1f76d
Jenkins <info@xtclang.org>
2022-06-22 13:24:33 -0400

    uploadXdk

--- xdk-latest.rb
+++ xdk-latest.rb
@@ -2,26 +2,26 @@ class XdkLatest < Formula
   desc "Ecstasy Development Kit (XDK)"
   homepage "https://github.com/xtclang/xvm/"
   url "http://xtclang.org/downloads/xdk-0.4.2ci46+71cd9cc5832038462d54cb0b84f4ce141e02b47f.tar.gz"
-  sha256 "b0b5217abf05de986e6211a7c8665b304fa89df1726749e4f1dd7eff1cbbe1fe"
   version "0.4.2.46"
+  sha256 "b0b5217abf05de986e6211a7c8665b304fa89df1726749e4f1dd7eff1cbbe1fe"
   license "Apache-2.0"
 
   depends_on "openjdk"
 
   on_linux do
-    OSNAME = "linux"
+    osname = "linux"
   end
 
   on_macos do
-    OSNAME = "macos"
+    osname = "macos"
   end
 
   def install
     libexec.mkpath
     cp_r Dir[buildpath/"*"], libexec
-    bin.install_symlink "#{libexec}/bin/#{OSNAME}_launcher" => "xec"
-    bin.install_symlink "#{libexec}/bin/#{OSNAME}_launcher" => "xtc"
-    bin.install_symlink "#{libexec}/bin/#{OSNAME}_launcher" => "xam"
+    bin.install_symlink "#{libexec}/bin/#{osname}_launcher" => "xec"
+    bin.install_symlink "#{libexec}/bin/#{osname}_launcher" => "xtc"
+    bin.install_symlink "#{libexec}/bin/#{osname}_launcher" => "xam"
   end
 
   test do
