bbcec3ac568a25130ed764a38efe0c3b967013b6
Jenkins <info@xtclang.org>
2023-11-08 08:02:52 -0500

    uploadXdk

--- xdk-latest.rb
+++ xdk-latest.rb
@@ -1,9 +1,9 @@
 class XdkLatest < Formula
   desc "Ecstasy Development Kit (XDK)"
   homepage "https://github.com/xtclang/xvm/"
-  url "http://xtclang.org/downloads/xdk-0.4.3ci810+47c5f0f17321d9c102da11fc1a23f60a702c7516.tar.gz"
-  version "0.4.3.810"
-  sha256 "3140aef5de5b807c8b2ca403ba897e405c0d95f8dea243d07091e639f09213a1"
+  url "http://xtclang.org/downloads/xdk-0.4.3ci811+0e89ff5be2bd8e201b0842c2f9b03304e4500385.tar.gz"
+  version "0.4.3.811"
+  sha256 "5c6019d9b060b4b9bd9e957c38f056e9388659a160937a158aa487571251bf00"
   license "Apache-2.0"
 
   depends_on "openjdk"
